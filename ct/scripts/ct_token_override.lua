ae7ce924f1680374b520d74530e7cfbb15e5c727
Chris Thiel <ccthiel@gmail.com>
2021-02-08 15:21:40 -0700

    Finally got it working

--- ct/scripts/ct_token_override.lua
+++ ct/scripts/ct_token_override.lua
@@ -15,23 +15,6 @@ function onHover(state)
 		tokenCT.addUnderlay(space, CombatEnhancer.TOKENUNDERLAYCOLOR_3); 
 
 	elseif tokenCT then
-		-- remove all underlays, if active then put back appropriate underlays
-		tokenCT.removeAllUnderlays(); 
-		if nodeActiveCT and nodeActiveCT.getNodeName() == nodeCT.getNodeName() then
-			local tokenActiveCT = CombatManager.getTokenFromCT(nodeActiveCT);
-			if tokenActiveCT then
-				local space = nodeActiveCT.getChild('space');  
-				if space == nil then 
-					space = 1;
-				else
-					space = space.getValue()/5/2+0.5; 
-				end
-				tokenCT.addUnderlay(space, CombatEnhancer.TOKENUNDERLAYCOLOR_1); 
-			end
-		else 
-			-- Token is not active in CT. Restore all other underlays.
-			Debug.console("Not active token");
-			TokenManager.updateSizeHelper(tokenCT, nodeCT);
-		end
+		TokenManager.updateSizeHelper(tokenCT, nodeCT);
 	end
 end
